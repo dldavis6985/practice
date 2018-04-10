0098f27025a212a6951e5b8c5b8af0e6f45650f7
Daniel Davis <dldavis6985@gmail.com>
2018-04-10 13:42:18 +1200

    Adding a c sharp practice folder

---
+++ CSharpPractice/MergeSort.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CSharpPractice
+{
+   
+    class MergeSort
+    {
+        
+        
+
+        public static void MergeSortIntList(List<int> toSort)
+        {   
+            int max = toSort.Count;
+
+            List<int> temp = new List<int>();
+            for(int i = 0; i < max; i ++)
+            {
+                temp.Add(0);
+            }
+            recursiveMerge(toSort, temp, 0, max-1);
+        }
+
+        public static void recursiveMerge(List<int> toSort, List<int> temp, int leftStart, int rightEnd)
+        {
+            if(leftStart >= rightEnd)
+            {
+                return;
+            }
+            int middle = (leftStart + rightEnd) / 2;
+            recursiveMerge(toSort, temp, leftStart, middle);
+            recursiveMerge(toSort, temp, middle + 1, rightEnd);
+            MergeHalves(toSort, temp, leftStart, rightEnd);
+        }
+
+        public static void MergeHalves(List<int> toSort, List<int> temp, int leftStart, int rightEnd)
+        {
+            int leftEnd = (leftStart + rightEnd) / 2;
+            int rightStart = leftEnd + 1;
+            int size = rightEnd - leftStart + 1;
+
+            int left = leftStart;
+            int right = rightStart;
+            int index = leftStart;
+
+            while(left <= leftEnd && right <= rightEnd)
+            {
+                if(toSort[left] <= toSort[right])
+                {
+                    temp[index] = toSort[left];
+                    left++;
+                }
+                else
+                {
+                    temp[index] = toSort[right];
+                    right++;
+                }
+                index++;
+            }
+
+            if(left <= leftEnd)
+            {
+                CopyList(temp, index, toSort, left, leftEnd - left+1);
+            }
+            if(right <= rightEnd)
+            {
+                CopyList(temp, index, toSort, right, rightEnd - right+1);
+            }
+            
+            
+
+            CopyList(toSort, leftStart, temp, leftStart, size);
+        }
+        public static void CopyList(List<int> targetList, int targetListPosition, List<int> copyList, int copyListPosition, int numberOfElements)
+        {
+            /*
+            CopyList copies a section of a list<int> into another list<int>
+
+            targetList is the list that will have elements copied into it.
+            targetListPosition is the index that "targetList" will begin to recieve copied elements at
+            copyList is the list that will have elements copied from
+            copyListPosition is the index it will copy from
+            numberOfElements is the ammount of elements that will be copied
+            */
+            if((targetListPosition + numberOfElements - 1)< targetList.Count && (copyListPosition + numberOfElements - 1) < copyList.Count)
+            {
+                for(int i = 0; i < numberOfElements; i++)
+                {
+                   targetList[targetListPosition + i] = copyList[copyListPosition + i];
+                }
+            }
+            else
+            {
+                Console.WriteLine("Can not copy that many elements");
+            }
+        }
+
+    }
+    
+}
---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CSharpPractice
+{
+   
+    class Practice
+    {
+        
+        static void Main(string[] args)
+        {
+            //using rNG as shorthand for Random Number Generator
+            Random rNG = new Random();
+            var toSort = new List<int> {};
+            int counter = 0;
+
+            int minRandom = 0;
+            int maxRandom = 1000;
+            int numbersToSort = 100;
+
+            while(counter < numbersToSort)
+            {
+                toSort.Add(rNG.Next(minRandom, maxRandom));
+                counter++;
+            }
+
+            int county = 0;
+            foreach(var i in toSort)
+            {
+                Console.WriteLine($"{county}: {i}");
+                county++;
+            }
+            Console.WriteLine("--------------------");
+
+            MergeSort.MergeSortIntList(toSort);           
+            
+            foreach(var i in toSort)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        
+
+    }
+    
+}
